de557522463750a347e0356da5c536f3a29769d9
mizdra <pp.mizdra@gmail.com>
2023-01-14 19:20:05 +0900

    stylelint-no-unused-selectors 動かないけど設定だけ追加

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {
     'selector-class-pattern': '^([a-z][a-z0-9]*)(_[a-z0-9]+)*$', // snake_case
-    'plugin/no-unused-selectors': true,
+    'plugin/no-unused-selectors': true, // NOTE: This does not work. See https://github.com/nodaguti/stylelint-no-unused-selectors/issues/182.
   },
 };
