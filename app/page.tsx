726239175ff1add705278b46f3c4d1fcb19408af
mizdra <pp.mizdra@gmail.com>
2023-01-14 19:22:14 +0900

    余計な css を削除

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <div>
-          <Image src={wave} alt="mizdra のアイコン" className={styles.vercel_logo} width={100} height={100} priority />
+          <Image src={wave} alt="mizdra のアイコン" width={100} height={100} priority />
         </div>
         <p>HNの読みは「みずどら」です</p>
       </div>
